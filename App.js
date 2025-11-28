d9dba15563ae44735c1b6a978d934518a30b6fe7
anirudh <2400031273@kluniversity.in>
2025-11-28 19:33:48 +0530

    Create App.js with Login and Register forms

---
+++ App.js
@@ -0,0 +1,14 @@
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <LoginForm />
+      <hr />
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
